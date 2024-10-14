9e35afba43d8632d9db5f94235fcd314259fb93a
bignatezzz <68629636+bignatezzz@users.noreply.github.com>
2024-10-14 11:06:28 -0700

    Create 1127-NthTribonacciNumber.py

---
+++ Arrays/1127-NthTribonacciNumber.py
@@ -0,0 +1,22 @@
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        if (n==0):
+            return 0
+        if (n==1):
+            return 1
+
+        if n == 2:
+            return 1
+
+        f0 = 0
+        f1 = 1
+        f2 = 1
+
+        for i in range(3,n+1):
+
+            sum = f0 + f1 + f2
+            f0 = f1
+            f1 = f2
+            f2 = sum
+
+        return f2
